3787a757ae153c31fea371782e59f5a829dd7dac
Aamir brar <amir@apporio.com>
2018-04-23 15:45:22 +0530

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./model/user');
+require('./services/passport');
+const authRoutes = require('./routes/authRoute');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send({bye: 'buddy'});
-});
-
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+authRoutes(app);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`server running at ${PORT}`);
